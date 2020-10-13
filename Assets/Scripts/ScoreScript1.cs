7b75314b766a651b8d40fa9179ad79c07c83d796
lucarl <carl.e.w.lundborg@gmail.com>
2020-10-13 15:07:55 +0200

    working two player with points and HR

---
+++ Assets/Scripts/ScoreScript1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript1 : MonoBehaviour
+{
+
+    Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Player 1: " + Player1.points;
+    }
+}
